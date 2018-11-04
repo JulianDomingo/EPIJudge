6776c8098a881a315a8ad10eaef2bcee899b8e95
JulianDomingo <julian.domingo@utexas.edu>
2018-11-04 14:30:50 -0600

    Finished 5.5, 5.6

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -2,11 +2,26 @@ from test_framework import generic_test
 
 
 def can_reach_end(A):
-    # TODO - you fill in here.
-    return True
+	# If we keep track of the furthest index we can traverse to as you iterate
+	# across the array, we know we can reach the end if A[len(A) - 2] is a value
+	# that is >= dst index (A(len(A) - 1]) is the destination index, so we don't
+	# count that).
+	furthest_index = 0
+	dst = len(A) - 1
+	i = 0
+
+	# Cannot continue if traversing index surpasses furthest_index, because this
+	# means you can't go any further than furthest_index. If you keep traversing,
+	# we are incorrectly assuming that the elements starting at index
+	# furthest_index + 1 are reachable
+	while i <= furthest_index and furthest_index < dst:
+		furthest_index = max(furthest_index, A[i] + i)
+		i += 1
+
+	return furthest_index >= dst
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            "advance_by_offsets.py", "advance_by_offsets.tsv", can_reach_end))
+	exit(
+		generic_test.generic_test_main(
+			"advance_by_offsets.py", "advance_by_offsets.tsv", can_reach_end))
--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,12 +2,17 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices):
-    # TODO - you fill in here.
-    return 0.0
+	cur_min, idx, res = prices[0], 1, 0.0
+
+	for i in range(1, len(prices)):
+		res = max(res, prices[i] - cur_min)
+		cur_min = min(cur_min, prices[i])
+
+	return res
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("buy_and_sell_stock.py",
-                                       "buy_and_sell_stock.tsv",
-                                       buy_and_sell_stock_once))
+	exit(
+		generic_test.generic_test_main("buy_and_sell_stock.py",
+									   "buy_and_sell_stock.tsv",
+									   buy_and_sell_stock_once))
