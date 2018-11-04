9badf047ddcb13fbeeeba2c1c9d6434e6a2acd50
JulianDomingo <julian.domingo@utexas.edu>
2018-11-04 17:31:24 -0600

    Finished 5.9

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -3,11 +3,23 @@ from test_framework import generic_test
 
 # Given n, return all primes up to and including n.
 def generate_primes(n):
-    # TODO - you fill in here.
-    return []
+	sieve = list(range(2, n + 1))
+
+	for i in range(len(sieve)):
+		if sieve[i] != -1:
+			sieve_step = sieve[i]
+			idx = i + sieve_step
+			# [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+			while idx < len(sieve):
+				# Clear out multiples of prime
+				sieve[idx] = -1
+				idx += sieve_step
+
+	return list(filter(lambda num: num != -1, sieve))
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("prime_sieve.py", "prime_sieve.tsv",
-                                       generate_primes))
+	exit(
+		generic_test.generic_test_main("prime_sieve.py", "prime_sieve.tsv",
+									   generate_primes))
