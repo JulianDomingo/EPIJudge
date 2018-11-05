b47f757ed6ec93014882e167f960e8011160f76a
JulianDomingo <julian.domingo@utexas.edu>
2018-11-04 23:39:54 -0600

    Finished 5.20

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -2,12 +2,18 @@ from test_framework import generic_test
 
 
 def generate_pascal_triangle(n):
-    # TODO - you fill in here.
-    return []
+	res = [[1] * (i + 1) for i in range(n)]
+
+	for i in range(n):
+		for j in range(1, i):
+			# We always skip the first column, since it's always 1
+			res[i][j] = res[i - 1][j - 1] + res[i - 1][j]
+
+	return res
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("pascal_triangle.py",
-                                       'pascal_triangle.tsv',
-                                       generate_pascal_triangle))
+	exit(
+		generic_test.generic_test_main("pascal_triangle.py",
+									   'pascal_triangle.tsv',
+									   generate_pascal_triangle))
