83c2f6378081ddabd169d5ebeda02eae50e4fa9f
JulianDomingo <julian.domingo@utexas.edu>
2018-11-06 00:27:37 -0600

    Finished 6.8

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,12 +1,29 @@
 from test_framework import generic_test
-
+from collections import Counter
 
 def look_and_say(n):
-    # TODO - you fill in here.
-    return ''
+	def next_las(s):
+		i, res = 0, []
+
+		while i < len(s):
+			count = 1
+			while i + 1 < len(s) and s[i] == s[i + 1]:
+				count += 1
+				i += 1
+
+			res.append(str(count) + str(s[i]))
+			i += 1
+
+		return "".join(res)
+
+	las = '1'
+	for i in range(1, n):
+		las = next_las(las)
+
+	return las
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("look_and_say.py", "look_and_say.tsv",
-                                       look_and_say))
+	exit(
+		generic_test.generic_test_main("look_and_say.py", "look_and_say.tsv",
+									   look_and_say))
