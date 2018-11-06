2e39b28f4b453e1af252f377535361d12c378589
JulianDomingo <julian.domingo@utexas.edu>
2018-11-05 22:13:43 -0600

    Finished 6.4, 6.5

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -2,12 +2,25 @@ from test_framework import generic_test
 
 
 def is_palindrome(s):
-    # TODO - you fill in here.
-    return True
+	start, end = 0, len(s) - 1
+
+	while start < end:
+		while start < end and not s[start].isalnum():
+			start += 1
+
+		while start < end and not s[end].isalnum():
+			end -= 1
+
+		if s[start].lower() != s[end].lower():
+			return False
+
+		start, end = start + 1, end - 1
+
+	return True
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("is_string_palindromic_punctuation.py",
-                                       "is_string_palindromic_punctuation.tsv",
-                                       is_palindrome))
+	exit(
+		generic_test.generic_test_main("is_string_palindromic_punctuation.py",
+									   "is_string_palindromic_punctuation.tsv",
+									   is_palindrome))
--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -5,18 +5,47 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size, s):
-    # TODO - you fill in here.
-    return 0
+	# Forward pass for determining total size of new string and skipping over
+	# 'b', second backward pass for overwriting 'a' with 'dd'
+	a_cnt = 0
+	write_idx = 0
+
+	for i in range(size):
+		if s[i] != 'b':
+			s[write_idx] = s[i]
+			write_idx += 1
+		if s[i] == 'a':
+			a_cnt += 1
+
+	# -1 since it write_idx will point to next available write space. -1 will
+	# move it back to last valid character
+	traverse = write_idx - 1
+	write_idx += a_cnt - 1
+	final_size = write_idx + 1
+
+	while traverse >= 0:
+		if s[traverse] == 'a':
+			s[write_idx - 1:write_idx + 1] = 'dd'
+			write_idx -= 2
+
+		else:
+			# Non 'a' character
+			s[write_idx] = s[traverse]
+			write_idx -= 1
+
+		traverse -= 1
+
+	return final_size
 
 
 @enable_executor_hook
 def replace_and_remove_wrapper(executor, size, s):
-    res_size = executor.run(functools.partial(replace_and_remove, size, s))
-    return s[:res_size]
+	res_size = executor.run(functools.partial(replace_and_remove, size, s))
+	return s[:res_size]
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("replace_and_remove.py",
-                                       'replace_and_remove.tsv',
-                                       replace_and_remove_wrapper))
+	exit(
+		generic_test.generic_test_main("replace_and_remove.py",
+									   'replace_and_remove.tsv',
+									   replace_and_remove_wrapper))
