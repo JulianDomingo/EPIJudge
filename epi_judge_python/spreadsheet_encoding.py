01669e327db5a181b54e1afe54f47f93652b3464
JulianDomingo <julian.domingo@utexas.edu>
2018-11-05 21:05:47 -0600

    Finished 6.1, 6.3

--- epi_judge_python/spreadsheet_encoding.py
+++ epi_judge_python/spreadsheet_encoding.py
@@ -2,12 +2,24 @@ from test_framework import generic_test
 
 
 def ss_decode_col_id(col):
-    # TODO - you fill in here.
-    return 0
+	# Can treat it as base 26 (hexadecimal is from A-F, so spreadsheet is from
+	# A-Z)
+	# AA => 1 * 26^1 + 1 * 26^0
+	import string
+	col_id = 0
+
+	BASE = 26
+	exp = 0
+
+	for char in reversed(col):
+		col_id += (string.ascii_uppercase.index(char) + 1) * BASE ** exp
+		exp += 1
+
+	return col_id
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("spreadsheet_encoding.py",
-                                       'spreadsheet_encoding.tsv',
-                                       ss_decode_col_id))
+	exit(
+		generic_test.generic_test_main("spreadsheet_encoding.py",
+									   'spreadsheet_encoding.tsv',
+									   ss_decode_col_id))
--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,24 +3,50 @@ from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x):
-    # TODO - you fill in here.
-    return ''
+	if x == 0:
+		return '0'
+
+	s = []
+
+	is_neg = x < 0
+	x = abs(x)
+
+	# 123 -> "321"
+	while x:
+		# We know '0' is our base number, so we add the offset based on the ones
+		# place digit
+		s.append(chr(ord('0') + x % 10))
+		x //= 10
+
+	s = "".join(reversed(s))
+	return "-" + s if is_neg else s
 
 
 def string_to_int(s):
-    # TODO - you fill in here.
-    return 0
+	import string
+
+	is_neg = s[0] == '-'
+	start = 1 if is_neg else 0
+
+	res = 0
+
+	# 13
+	for i in range(start, len(s)):
+		res *= 10
+		res += string.digits.index(s[i])
+
+	return -res if is_neg else res
 
 
 def wrapper(x, s):
-    if int_to_string(x) != s:
-        raise TestFailure("Int to string conversion failed")
-    if string_to_int(s) != x:
-        raise TestFailure("String to int conversion failed")
+	if int_to_string(x) != s:
+		raise TestFailure("Int to string conversion failed")
+	if string_to_int(s) != x:
+		raise TestFailure("String to int conversion failed")
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("string_integer_interconversion.py",
-                                       'string_integer_interconversion.tsv',
-                                       wrapper))
+	exit(
+		generic_test.generic_test_main("string_integer_interconversion.py",
+									   'string_integer_interconversion.tsv',
+									   wrapper))
