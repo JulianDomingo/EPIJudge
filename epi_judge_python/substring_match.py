11c3a0cd019b3f4923a43cc15242e2ce181e2975
JulianDomingo <julian.domingo@utexas.edu>
2018-11-06 21:55:47 -0600

    Finished rabin-karp 6.13

--- epi_judge_python/substring_match.py
+++ epi_judge_python/substring_match.py
@@ -1,12 +1,54 @@
 from test_framework import generic_test
 
+def hash_apppend(existing_hash, to_append):
+	existing_hash = (existing_hash * BASE) + ord(to_append)
+	return existing_hash % PRIME
+
+
+def hash_pop(existing_hash, to_pop):
+	existing_hash -= (ord(to_pop) * (BASE**(S_LEN - 1) % PRIME))
+	return existing_hash % PRIME
+
 
 def rabin_karp(t, s):
-    # TODO - you fill in here.
-    return 0
+	if len(s) > len(t):
+		return -1
 
+	global BASE, PRIME, S_LEN
+	# We assume we are only given a character set of alphabetical characters.
+	# Thus, a base of 27 is unique.
+	BASE = 255
+	# Should be a prime
+	PRIME = 97
+	S_LEN = len(s)
+	s_hash, cur_hash = 0, 0
+
+	# Calculate hash to find
+	for i in range(len(s)):
+		s_hash = hash_apppend(s_hash, s[i])
+
+	# Calculate starting hash
+	for i in range(len(t[:S_LEN])):
+		cur_hash = hash_apppend(cur_hash, t[i])
+
+	# Find the first occurrence of string 's' in text 't'
+	# Sliding window lmao
+	n = len(s)
+	st, end = 0, n
+
+	while end < len(t):
+		cur_hash = hash_pop(cur_hash, t[st])
+		cur_hash = hash_apppend(cur_hash, t[end])
+		st, end = st + 1, end + 1
+
+		if cur_hash == s_hash:
+			# Check actual strings in case of hash collision
+			print("Checking {} and {}".format(s, t[st:end]))
+			return st
+
+	return -1
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("substring_match.py",
-                                       'substring_match.tsv', rabin_karp))
+	exit(
+		generic_test.generic_test_main("substring_match.py",
+									   'substring_match.tsv', rabin_karp))
--- epi_judge_python/test.py
+++ epi_judge_python/test.py
@@ -1,26 +1,72 @@
-def dutch_flag_partition(pivot_index, A):
-	pivot_value = A[pivot_index]
+# def dutch_flag_partition(pivot_index, A):
+	# pivot_value = A[pivot_index]
 
-	smaller, larger = 0, len(A) - 1
+	# smaller, larger = 0, len(A) - 1
 
-	for i in range(len(A)):
-		if A[i] < pivot_value:
-			A[i], A[smaller] = A[smaller], A[i]
-			smaller += 1
-
-
-	for j in reversed(range(len(A))):
-		if A[j] > pivot_value:
-			A[j], A[larger] = A[larger], A[j]
-			larger -= 1
-
-	print(str(A))
-
-	return
+	# for i in range(len(A)):
+		# if A[i] < pivot_value:
+			# A[i], A[smaller] = A[smaller], A[i]
+			# smaller += 1
 
 
-A = [0, 1, 2, 0, 2, 1, 1]
+	# for j in reversed(range(len(A))):
+		# if A[j] > pivot_value:
+			# A[j], A[larger] = A[larger], A[j]
+			# larger -= 1
 
-dutch_flag_partition(2, A)
+	# print(str(A))
 
-print(str(A))
+	# return
+
+
+# A = [0, 1, 2, 0, 2, 1, 1]
+
+# dutch_flag_partition(2, A)
+
+# print(str(A))
+
+def rabin_karp(t, s):
+	if len(s) > len(t):
+		return -1
+
+	# TODO: You were on right track, but ord() doesn't take into consideration
+	# ordering of the string. Use O(1) hashing of the string s and compare the
+	# hashing of the current len(s) size window. If hashes match, you found the
+	# result
+
+	# Find the first occurrence of string 's' in text 't'
+	# Sliding window lmao
+	n = len(s)
+	st, end = 0, n
+
+	# We get the ord() sum of 's'. When we traverse through the string with
+	# sliding window of size len(s), we add the ord() of the new window suffix
+	# and subtract the ord() of the window prefix
+	# The result is the index of "st"
+
+	# Ex. s='ACAIBOWL', t='BOWL'
+	s_ord = sum([ord(c) for c in s])
+
+	print('ord() of {}: {}'.format(s, str(s_ord)))
+
+	# We initialize our window to the first len(s) characters in the string t
+	cur_ord = sum([ord(c) for c in t[:n]])
+
+	if cur_ord == s_ord:
+		return 0
+
+	while end < len(t):
+		cur_ord -= ord(t[st])
+		cur_ord += ord(t[end])
+		st, end = st + 1, end + 1
+
+		print("Testing string {} with score {}".format(t[st:end], str(cur_ord)))
+
+		if cur_ord == s_ord:
+			return st
+
+	return -1
+
+t = 'babababbaabaabbbbb'
+s = 'babba'
+rabin_karp(t, s)
