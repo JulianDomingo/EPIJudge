bd397793cbc2c2f2f926176fb1a0eb630f21892a
JulianDomingo <julian.domingo@utexas.edu>
2018-11-07 00:59:55 -0600

    Finished 7.7

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -1,14 +1,35 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L, k):
-    # TODO - you fill in here.
-    return None
+	# Two pointers. One trails the list normally, other is exactly k nodes behind
+	# it. When normal pointer reaches end, trailing pointer points to the node
+	# we want to delete.
+	trailing = None
+	dummy_head = ListNode(0, L)
+	cur = dummy_head.next
+
+	for i in range(k):
+		cur = cur.next
+
+	# 1 -> 2 -> 3 -> 4 -> 5, k = 2 (delete 4)
+	# 2 -> 1, k = 2 (delete 2)
+
+	trailing = dummy_head
+
+	while cur:
+		cur, trailing = cur.next, trailing.next
+
+	# Trailing points to node to delete
+	trailing.next = trailing.next.next
+
+	return dummy_head.next
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("delete_kth_last_from_list.py",
-                                       'delete_kth_last_from_list.tsv',
-                                       remove_kth_last))
+	exit(
+		generic_test.generic_test_main("delete_kth_last_from_list.py",
+									   'delete_kth_last_from_list.tsv',
+									   remove_kth_last))
