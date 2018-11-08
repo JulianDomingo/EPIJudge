73c4d60bec8542855805082a78d0b620fe9ce152
JulianDomingo <julian.domingo@utexas.edu>
2018-11-07 20:12:51 -0600

    Finished 7.8

--- epi_judge_python/remove_duplicates_from_sorted_list.py
+++ epi_judge_python/remove_duplicates_from_sorted_list.py
@@ -1,13 +1,29 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def remove_duplicates(L):
-    # TODO - you fill in here.
-    return None
+	dummy = ListNode(0, L)
+	dummy.next = L
+
+	while L and L.next:
+		# There can be more than one duplicate
+		if L.data == L.next.data:
+			tmp = L.next
+
+			while tmp.next and tmp.data == tmp.next.data:
+				tmp = tmp.next
+
+			L.next = tmp.next
+
+		else:
+			L = L.next
+
+	return dummy.next
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            "remove_duplicates_from_sorted_list.py",
-            'remove_duplicates_from_sorted_list.tsv', remove_duplicates))
+	exit(
+		generic_test.generic_test_main(
+			"remove_duplicates_from_sorted_list.py",
+			'remove_duplicates_from_sorted_list.tsv', remove_duplicates))
