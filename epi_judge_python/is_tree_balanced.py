e725f1ba6f33b63a7f7e529ac88d937b7746e76d
JulianDomingo <julian.domingo@utexas.edu>
2018-11-10 17:04:17 -0600

    Finished 9.1, 9.2

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,13 +1,55 @@
 from test_framework import generic_test
 
+#
+#	  1
+#	 /
+#	2
+#
+#	  1
+#	 / \
+#	2	3
+
+#
+#	  1
+#	 / \
+#	3	4
+#  /
+# 2
+#
+#
+#
+#
+
 
 def is_balanced_binary_tree(tree):
-    # TODO - you fill in here.
-    return True
+	if not tree:
+		return True
+
+	height, is_bal = helper(tree)
+
+	return is_bal
+
+
+def helper(root):
+	if not root:
+		return 0, True
+
+	left_h, left_is_bal = helper(root.left)
+	right_h, right_is_bal = helper(root.right)
+
+	if not left_is_bal or not right_is_bal:
+		return 0, False
+
+	is_bal = abs(left_h - right_h) <= 1
+	return max(left_h, right_h) + 1, is_bal
+
+
+
+
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("is_tree_balanced.py",
-                                       'is_tree_balanced.tsv',
-                                       is_balanced_binary_tree))
+	exit(
+		generic_test.generic_test_main("is_tree_balanced.py",
+									   'is_tree_balanced.tsv',
+									   is_balanced_binary_tree))
--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -2,11 +2,30 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree):
-    # TODO - you fill in here.
-    return True
+	if not tree or not tree.left and not tree.right:
+		return True
+
+	return helper(tree.left, tree.right)
+
+
+def helper(left, right):
+	if not left and not right:
+		# Reached end of checking, know we are symmetric
+		return True
+
+	# If only once is none, invalid symmetric
+	if not left and right or left and not right:
+		return False
+
+	# Know we have 2 children at this point, so check their value and their
+	# respective subtrees
+	# Check if left's right subtree is equal to right's left subtree and vice
+	# versa
+	return (left.data == right.data and helper(left.left, right.right) and
+										helper(left.right, right.left))
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("is_tree_symmetric.py",
-                                       'is_tree_symmetric.tsv', is_symmetric))
+	exit(
+		generic_test.generic_test_main("is_tree_symmetric.py",
+									   'is_tree_symmetric.tsv', is_symmetric))
