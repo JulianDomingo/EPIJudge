af4ef979a1f8e20f79dac2340e3697c138ade68b
JulianDomingo <julian.domingo@utexas.edu>
2018-11-10 14:21:55 -0600

    Finished 8.7, 8.8

--- epi_judge_python/circular_queue.py
+++ epi_judge_python/circular_queue.py
@@ -3,46 +3,67 @@ from test_framework.test_failure import TestFailure
 
 
 class Queue:
-    def __init__(self, capacity):
-        # TODO - you fill in here.
-        return
+	SCALE_FACTOR = 2
 
-    def enqueue(self, x):
-        # TODO - you fill in here.
-        return
+	def __init__(self, capacity):
+		self.a = [None] * capacity
+		self.head = self.tail = self.cur_size = 0
+		return
 
-    def dequeue(self):
-        # TODO - you fill in here.
-        return 0
 
-    def size(self):
-        # TODO - you fill in here.
-        return 0
+	def enqueue(self, x):
+		# [1, 1, 1, 1]
+		if self.cur_size == len(self.a):
+			# Double capacity
+			self.a = self.a[self.head:] + self.a[:self.head]
+			self.head, self.tail = 0, self.cur_size
+			self.a += [None] * (len(self.a) * Queue.SCALE_FACTOR - len(self.a))
+
+		self.a[self.tail] = x
+		self.tail = (self.tail + 1) % len(self.a)
+		self.cur_size += 1
+
+		return
+
+
+	def dequeue(self):
+		if not self.cur_size:
+			raise IndexError('Empty queue!')
+
+		self.cur_size -= 1
+		to_return = self.a[self.head]
+		self.head = (self.head + 1) % len(self.a)
+
+		return to_return
+
+
+	def size(self):
+		return self.cur_size
 
 
 def queue_tester(ops):
-    q = Queue(1)
+	q = Queue(1)
 
-    for (op, arg) in ops:
-        if op == 'Queue':
-            q = Queue(arg)
-        elif op == 'enqueue':
-            q.enqueue(arg)
-        elif op == 'dequeue':
-            result = q.dequeue()
-            if result != arg:
-                raise TestFailure(
-                    "Dequeue: expected " + str(arg) + ", got " + str(result))
-        elif op == 'size':
-            result = q.size()
-            if result != arg:
-                raise TestFailure(
-                    "Size: expected " + str(arg) + ", got " + str(result))
-        else:
-            raise RuntimeError("Unsupported queue operation: " + op)
+	for (op, arg) in ops:
+		if op == 'Queue':
+			q = Queue(arg)
+		elif op == 'enqueue':
+			q.enqueue(arg)
+		elif op == 'dequeue':
+			result = q.dequeue()
+			if result != arg:
+				raise TestFailure(
+					"Dequeue: expected " + str(arg) + ", got " + str(result))
+		elif op == 'size':
+			result = q.size()
+			if result != arg:
+				raise TestFailure(
+					"Size: expected " + str(arg) + ", got " + str(result))
+		else:
+			raise RuntimeError("Unsupported queue operation: " + op)
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("circular_queue.py",
-                                       'circular_queue.tsv', queue_tester))
+	exit(
+		generic_test.generic_test_main("circular_queue.py",
+									   'circular_queue.tsv', queue_tester))
--- epi_judge_python/queue_from_stacks.py
+++ epi_judge_python/queue_from_stacks.py
@@ -2,38 +2,56 @@ from test_framework import generic_test
 
 
 class Queue:
-    def enqueue(self, x):
-        # TODO - you fill in here.
-        return
+	# Using stacks, one holds all elements enqueued. When we dequeue, we must
+	# pop everything from the stack and place it in a temp stack.
+	def __init__(self):
+		self.elems = []
+		self.temp = []
 
-    def dequeue(self):
-        # TODO - you fill in here.
-        return 0
+
+	def enqueue(self, x):
+		self.elems.append(x)
+
+
+	def dequeue(self):
+		if not self.elems:
+			raise IndexError('Queue is empty!')
+
+		while len(self.elems):
+			self.temp.append(self.elems.pop())
+
+		to_ret = self.temp.pop()
+
+		# Place back to elems
+		while len(self.temp):
+			self.elems.append(self.temp.pop())
+
+		return to_ret
 
 
 def queue_tester(ops):
-    from test_framework.test_failure import TestFailure
+	from test_framework.test_failure import TestFailure
 
-    try:
-        q = Queue()
+	try:
+		q = Queue()
 
-        for (op, arg) in ops:
-            if op == 'Queue':
-                q = Queue()
-            elif op == 'enqueue':
-                q.enqueue(arg)
-            elif op == 'dequeue':
-                result = q.dequeue()
-                if result != arg:
-                    raise TestFailure("Dequeue: expected " + str(arg) +
-                                      ", got " + str(result))
-            else:
-                raise RuntimeError("Unsupported queue operation: " + op)
-    except IndexError:
-        raise TestFailure('Unexpected IndexError exception')
+		for (op, arg) in ops:
+			if op == 'Queue':
+				q = Queue()
+			elif op == 'enqueue':
+				q.enqueue(arg)
+			elif op == 'dequeue':
+				result = q.dequeue()
+				if result != arg:
+					raise TestFailure("Dequeue: expected " + str(arg) +
+									  ", got " + str(result))
+			else:
+				raise RuntimeError("Unsupported queue operation: " + op)
+	except IndexError:
+		raise TestFailure('Unexpected IndexError exception')
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("queue_from_stacks.py",
-                                       'queue_from_stacks.tsv', queue_tester))
+	exit(
+		generic_test.generic_test_main("queue_from_stacks.py",
+									   'queue_from_stacks.tsv', queue_tester))
