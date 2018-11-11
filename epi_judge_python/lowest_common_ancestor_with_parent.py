0715f4dedd2f9b3ad244bd94d1b6f4c24bf569ff
JulianDomingo <julian.domingo@utexas.edu>
2018-11-10 18:32:54 -0600

    Finished 9.4, 9.5

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -7,23 +7,72 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def lca(node0, node1):
-    # TODO - you fill in here.
-    return None
+	# Get depths of node0 and node1
+	# Traverse upwards the deeper node until both nodes are at same layer
+	# First node where node0 and node1 are the same reference is the LCA
+
+	# Get depth
+	node0_depth = node1_depth = 0
+	tmp0, tmp1 = node0, node1
+
+	while tmp0:
+		node0_depth += 1
+		tmp0 = tmp0.parent
+
+	while tmp1:
+		node1_depth += 1
+		tmp1 = tmp1.parent
+
+	# Traverse deeper node abs(node0_depth - node1_depth)
+	diff = abs(node1_depth - node0_depth)
+
+	if node0_depth > node1_depth:
+		while diff:
+			node0 = node0.parent
+			diff -= 1
+	else:
+		while diff:
+			node1 = node1.parent
+			diff -= 1
+
+	# Traverse upwards in tandem
+	while node0 is not node1:
+		node0, node1 = node0.parent, node1.parent
+
+	# Return either arbitrarily
+	return node0
+
+	# Initial attempt
+	# if not node0 or not node1:
+		# return None
+
+	# while node0 is not node1:
+		# if not node0.data:
+			# return node0
+		# elif not node1.data:
+			# return node1
+
+		# node0.data = node1.data = None
+		# node0 = node0.parent if node0.parent else node0
+		# node1 = node1.parent if node1.parent else node1
+
+	# # Arbitrarily return node0 or node1
+	# return node0
 
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, node0, node1):
-    result = executor.run(
-        functools.partial(lca, must_find_node(tree, node0),
-                          must_find_node(tree, node1)))
+	result = executor.run(
+		functools.partial(lca, must_find_node(tree, node0),
+						  must_find_node(tree, node1)))
 
-    if result is None:
-        raise TestFailure("Result can't be None")
-    return result.data
+	if result is None:
+		raise TestFailure("Result can't be None")
+	return result.data
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("lowest_common_ancestor_with_parent.py",
-                                       'lowest_common_ancestor.tsv',
-                                       lca_wrapper))
+	exit(
+		generic_test.generic_test_main("lowest_common_ancestor_with_parent.py",
+									   'lowest_common_ancestor.tsv',
+									   lca_wrapper))
--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -2,11 +2,41 @@ from test_framework import generic_test
 
 
 def sum_root_to_leaf(tree, partial_path_sum=0):
-    # TODO - you fill in here.
-    return 0
+	res = [0]
+	helper(0, res, tree)
+	return res[0]
+
+
+def helper(cur, res, root):
+	if not root:
+		return
+	if leaf(root):
+		cur <<= 1
+		cur |= root.data
+		res[0] += b2d(cur)
+	else:
+		cur <<= 1
+		cur |= root.data
+		helper(cur, res, root.left)
+		helper(cur, res, root.right)
+
+
+def b2d(val):
+	res = 0
+	exp = 0
+	while val:
+		res += (val & 1) << exp
+		val >>= 1
+		exp += 1
+
+	return res
+
+
+def leaf(cur):
+	return not cur.left and not cur.right
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            "sum_root_to_leaf.py", 'sum_root_to_leaf.tsv', sum_root_to_leaf))
+	exit(
+		generic_test.generic_test_main(
+			"sum_root_to_leaf.py", 'sum_root_to_leaf.tsv', sum_root_to_leaf))
