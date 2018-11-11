4d7580e16d1d17db5aafa933e7576687782115d8
JulianDomingo <julian.domingo@utexas.edu>
2018-11-11 16:46:54 -0600

    Updated 9.12

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -3,34 +3,18 @@ from binary_tree_node import BinaryTreeNode
 
 
 def binary_tree_from_preorder_inorder(preorder, inorder):
-	# Generate subtrees recursively by keep track of the indices. This is linear
-	# time, since we traverse each element only once.
-	inorder_node_idxs = {data: idx for data, idx in enumerate(inorder)}
+	if inorder:
+		root_idx = inorder.index(preorder.pop(0))
+		root = BinaryTreeNode(data=inorder[root_idx])
 
-	preorder_iter = iter(preorder)
-
-	def helper(start, end):
-		if start > end:
-			return None
-
-		# Find current subtree root in inorder list
-		root_val = next(preorder_iter)
-		root = BinaryTreeNode()
-		root.data = root_val
-		root_idx = inorder_node_idxs[root_val]
-
-		# In an inorder list, the left subtree ENDS at the left of the root,
-		# and the right subtree STARTS at the right of the root.
-
-		# Generate left subtree
-		root.left = helper(start, root_idx - 1)
-
-		# Generate right subtree
-		root.right = helper(root_idx + 1, end)
+		# For an inorder serialized list, the left subtree of the current root
+		# is always to the left of the index of current root, and the right
+		# subtree of the current root is always to the right of the index + 1
+		# of the current root
+		root.left = binary_tree_from_preorder_inorder(preorder, inorder[:root_idx])
+		root.right = binary_tree_from_preorder_inorder(preorder, inorder[root_idx + 1:])
 
 		return root
-
-	return helper(0, len(inorder) - 1)
 
 
 if __name__ == '__main__':
