3f52e499f82532b8a869a83fdc39925c5b9f2e14
JulianDomingo <julian.domingo@utexas.edu>
2018-11-10 18:52:27 -0600

    Finished 9.7

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -2,11 +2,30 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree):
-    # TODO - you fill in here.
-    return []
+	res = []
+
+	while tree:
+		if not tree.left:
+			res.append(tree.data)
+			tree = tree.right
+		else:
+			# Find inorder predecessor
+			ip = tree.left
+			while ip and ip.right and ip.right is not tree:
+				ip = ip.right
+
+			if ip.right is tree:
+				ip.right = None
+				res.append(tree.data)
+				tree = tree.right
+			else:
+				ip.right = tree
+				tree = tree.left
+
+	return res
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("tree_inorder.py", 'tree_inorder.tsv',
-                                       inorder_traversal))
+	exit(
+		generic_test.generic_test_main("tree_inorder.py", 'tree_inorder.tsv',
+									   inorder_traversal))
