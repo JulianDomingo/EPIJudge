144bfdbacd603932716b8187cc7e2fc477cd63bb
JulianDomingo <julian.domingo@utexas.edu>
2018-11-11 17:37:55 -0600

    Finished 9.15

--- epi_judge_python/tree_connect_leaves.py
+++ epi_judge_python/tree_connect_leaves.py
@@ -6,21 +6,34 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def create_list_of_leaves(tree):
-    # TODO - you fill in here.
-    return []
+	res = []
+
+	def inorder(root, res):
+		if not root:
+			return
+
+		if not root.left and not root.right:
+			res.append(root)
+			return
+
+		inorder(root.left, res)
+		inorder(root.right, res)
+
+	inorder(tree, res)
+	return res
 
 
 @enable_executor_hook
 def create_list_of_leaves_wrapper(executor, tree):
-    result = executor.run(functools.partial(create_list_of_leaves, tree))
+	result = executor.run(functools.partial(create_list_of_leaves, tree))
 
-    if any(x is None for x in result):
-        raise TestFailure("Result list can't contain None")
-    return [x.data for x in result]
+	if any(x is None for x in result):
+		raise TestFailure("Result list can't contain None")
+	return [x.data for x in result]
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("tree_connect_leaves.py",
-                                       "tree_connect_leaves.tsv",
-                                       create_list_of_leaves_wrapper))
+	exit(
+		generic_test.generic_test_main("tree_connect_leaves.py",
+									   "tree_connect_leaves.tsv",
+									   create_list_of_leaves_wrapper))
--- epi_judge_python/tree_exterior.py
+++ epi_judge_python/tree_exterior.py
@@ -5,25 +5,47 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
+def is_leaf(root):
+	return not root.left and not root.right
+
+
 def exterior_binary_tree(tree):
-    # TODO - you fill in here.
-    return []
+	def left_subtree(subtree, is_boundary):
+		if not subtree:
+			return []
+
+		return (([subtree] if (is_boundary or is_leaf(subtree)) else []) +
+				left_subtree(subtree.left, is_boundary) +
+				left_subtree(subtree.right, is_boundary and not subtree.left))
+
+
+	def right_subtree(subtree, is_boundary):
+		if not subtree:
+			return []
+
+		return (right_subtree(subtree.left, is_boundary and not subtree.right) +
+				right_subtree(subtree.right, is_boundary) +
+			   ([subtree] if (is_boundary or is_leaf(subtree)) else []))
+
+
+	return (tree + left_subtree(tree.left, is_boundary=True) +
+				   right_subtree(tree.right, is_boundary=True) if tree else [])
 
 
 def create_output_list(L):
-    if any(l is None for l in L):
-        raise TestFailure('Resulting list contains None')
-    return [l.data for l in L]
+	if any(l is None for l in L):
+		raise TestFailure('Resulting list contains None')
+	return [l.data for l in L]
 
 
 @enable_executor_hook
 def create_output_list_wrapper(executor, tree):
-    result = executor.run(functools.partial(exterior_binary_tree, tree))
+	result = executor.run(functools.partial(exterior_binary_tree, tree))
 
-    return create_output_list(result)
+	return create_output_list(result)
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("tree_exterior.py", 'tree_exterior.tsv',
-                                       create_output_list_wrapper))
+	exit(
+		generic_test.generic_test_main("tree_exterior.py", 'tree_exterior.tsv',
+									   create_output_list_wrapper))
