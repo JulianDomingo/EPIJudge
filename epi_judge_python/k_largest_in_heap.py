43bbe39f7c7b48d7fb73c58535b456a5e298ac4b
JulianDomingo <julian.domingo@utexas.edu>
2018-11-12 00:15:39 -0600

    Finished ch. 10

--- epi_judge_python/k_largest_in_heap.py
+++ epi_judge_python/k_largest_in_heap.py
@@ -1,15 +1,34 @@
 from test_framework import generic_test, test_utils
+import heapq
 
 
 def k_largest_in_binary_heap(A, k):
-    # TODO - you fill in here.
-    return []
+	# O(k * log(k)) time, O(k) space for heap
+	i_am_left = False
+	res, max_heap = [], []
+
+	heapq.heappush(max_heap, (-A[0], 0))
+
+	for _ in range(k):
+		next_max, next_max_idx = heapq.heappop(max_heap)
+		res.append(-next_max)
+
+		# At most k nodes in heap
+		left_child_idx = (2 * next_max_idx) + 1
+		if (left_child_idx < len(A)):
+			heapq.heappush(max_heap, (-A[left_child_idx], left_child_idx))
+
+		right_child_idx = (2 * next_max_idx) + 2
+		if (right_child_idx < len(A)):
+			heapq.heappush(max_heap, (-A[right_child_idx], right_child_idx))
+
+	return res
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            "k_largest_in_heap.py",
-            "k_largest_in_heap.tsv",
-            k_largest_in_binary_heap,
-            comparator=test_utils.unordered_compare))
+	exit(
+		generic_test.generic_test_main(
+			"k_largest_in_heap.py",
+			"k_largest_in_heap.tsv",
+			k_largest_in_binary_heap,
+			comparator=test_utils.unordered_compare))
--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,16 +1,48 @@
 from test_framework import generic_test
+import heapq
 
 
 def online_median(sequence):
-    # TODO - you fill in here.
-    return []
+	# Use min heap to store elements after median and max heap to store elements
+	# before the median. Then, the root of each heap averaged out is the
+	# median.
+	# O(log(N)), where N is the size of the sequence for insertions
+
+	min_h, max_h = [], []
+	res = []
+
+	store_min = True
+
+	# 5, 4, 3, 2, 1
+	# min: [5] max: []
+	# min: [5] max: [4] => 4.5
+	# min: [3, 5] max: [4] => 4
+
+	for num in sequence:
+		if store_min:
+			heapq.heappush(min_h, num)
+		else:
+			heapq.heappush(max_h, -num)
+
+		store_min = not store_min
+
+		# Compute median
+		if not max_h:
+			res.append(min_h[0])
+		elif len(max_h) == len(min_h):
+			res.append((min_h[0] + (-max_h[0])) * 0.5)
+		else:
+			# Odd number of elements, return root of min_h
+			res.append(max(-max_h[0], min_h[0]))
+
+	return res
 
 
 def online_median_wrapper(sequence):
-    return online_median(iter(sequence))
+	return online_median(iter(sequence))
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("online_median.py", "online_median.tsv",
-                                       online_median_wrapper))
+	exit(
+		generic_test.generic_test_main("online_median.py", "online_median.tsv",
+									   online_median_wrapper))
