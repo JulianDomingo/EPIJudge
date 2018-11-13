278c73b16d4642024d83ab3b925b8b8a0302e0a5
JulianDomingo <julian.domingo@utexas.edu>
2018-11-12 20:02:15 -0600

    Finished 11.1

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,11 +2,30 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+	# Do regular binary search, but keep continuously updating the result every
+	# time you half the input size if the midpoint is still the value 'k' that
+	# I'm looking for.
+	first_idx = -1
+	s, e = 0, len(A) - 1
+
+	# 0, 1, 2, 3, 4, 5, 6, 7
+
+	while s <= e:
+		mid = (s + e) // 2
+
+		if A[mid] == k:
+			first_idx = mid
+			e = mid - 1
+		elif A[mid] < k:
+			s = mid + 1
+		else:
+			# A[mid] > k
+			e = mid - 1
+
+	return first_idx
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            "search_first_key.py", 'search_first_key.tsv', search_first_of_k))
+	exit(
+		generic_test.generic_test_main(
+			"search_first_key.py", 'search_first_key.tsv', search_first_of_k))
