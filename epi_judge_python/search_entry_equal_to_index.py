c6569f99698c743603b81805455c6c4153827d2d
JulianDomingo <julian.domingo@utexas.edu>
2018-11-12 20:11:49 -0600

    Finished 11.2

--- epi_judge_python/search_entry_equal_to_index.py
+++ epi_judge_python/search_entry_equal_to_index.py
@@ -6,25 +6,40 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def search_entry_equal_to_its_index(A):
-    # TODO - you fill in here.
-    return 0
+	# [-1, 1, 5, 6, 7]
+	entry_equal_to_idx = -1
+	s, e = 0, len(A) - 1
+
+	while s <= e:
+		mid = (s + e) // 2
+
+		if A[mid] == mid:
+			entry_equal_to_idx = A[mid]
+			break
+		elif A[mid] > mid:
+			e = mid - 1
+		else:
+			# A[mid] < mid
+			s = mid + 1
+
+	return entry_equal_to_idx
 
 
 @enable_executor_hook
 def search_entry_equal_to_its_index_wrapper(executor, A):
-    result = executor.run(
-        functools.partial(search_entry_equal_to_its_index, A))
-    if result != -1:
-        if A[result] != result:
-            raise TestFailure("Entry does not equal to its index")
-    else:
-        if any(i == a for i, a in enumerate(A)):
-            raise TestFailure("There are entries which equal to its index")
+	result = executor.run(
+		functools.partial(search_entry_equal_to_its_index, A))
+	if result != -1:
+		if A[result] != result:
+			raise TestFailure("Entry does not equal to its index")
+	else:
+		if any(i == a for i, a in enumerate(A)):
+			raise TestFailure("There are entries which equal to its index")
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            "search_entry_equal_to_index.py",
-            'search_entry_equal_to_index.tsv',
-            search_entry_equal_to_its_index_wrapper))
+	exit(
+		generic_test.generic_test_main(
+			"search_entry_equal_to_index.py",
+			'search_entry_equal_to_index.tsv',
+			search_entry_equal_to_its_index_wrapper))
